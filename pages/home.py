891ac6ee09e7bc1330150f9c7fd495b2ffda09e4
rgab05 <98488201+rgab05@users.noreply.github.com>
2025-12-07 22:57:06 +0000

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.charts import create_map, create_bar_chart
 
 def show(df):
-    st.title("🌍 Global Data Dashboard")
+    st.title("Global Data Dashboard")
 
     selected_country = st.sidebar.selectbox(
         "🔍 Select Country (Drill-down)",
@@ -13,23 +13,23 @@ def show(df):
     col1, col2, col3 = st.columns(3)
     if selected_country != "All":
         df_country = df[df["country"] == selected_country]
-        col1.metric("🌎 Country", selected_country)
-        col2.metric("❤️ Max Life Expectancy", round(df_country["lifeExp"].max(), 1))
-        col3.metric("📊 Population (2007)", int(df_country[df_country["year"]==2007]["pop"].values[0]))
+        col1.metric("Country", selected_country)
+        col2.metric("Max Life Expectancy", round(df_country["lifeExp"].max(), 1))
+        col3.metric("Population (2007)", int(df_country[df_country["year"]==2007]["pop"].values[0]))
     else:
-        col1.metric("🌎 Total Countries", df["country"].nunique())
-        col2.metric("📊 Total Records", len(df))
-        col3.metric("❤️ Max Life Expectancy", round(df["lifeExp"].max(), 1))
+        col1.metric("Total Countries", df["country"].nunique())
+        col2.metric("Total Records", len(df))
+        col3.metric("Max Life Expectancy", round(df["lifeExp"].max(), 1))
 
     st.divider()
 
     # MAP + BAR CHART
     left, right = st.columns([1.3, 1])
     with left:
-        st.subheader("🗺️ Global Life Expectancy Map")
+        st.subheader("Global Life Expectancy Map")
         st.plotly_chart(create_map(df, selected_country), use_container_width=True)
     with right:
-        st.subheader("📊 Life Expectancy by Continent / Country")
+        st.subheader("Life Expectancy by Continent / Country")
         st.plotly_chart(create_bar_chart(df, selected_country), use_container_width=True)
 
     st.divider()
